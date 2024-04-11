f375392484be59039f0765c3268d9b34ea5acd62
pabloieshna <joesingleton417@gmail.com>
2024-04-11 10:47:27 +0200

    solucion a la conversion numero-caracter

--- src/ejercicios/Ejercicio03.java
+++ src/ejercicios/Ejercicio03.java
@@ -6,14 +6,16 @@ public class Ejercicio03 {
 public static void main(String[] args) {
 	Scanner leer=new Scanner(System.in);
 	char cosa;
+	String line;
 	byte nextInt = 0;
-	try {
-		nextInt= (byte) leer.nextInt();
-		cosa=(char) nextInt;
-	} catch (Exception e) {
-		byte conversor=(byte) nextInt;
-		cosa=(char) conversor;
-	}
+		line= leer.nextLine();
+		try {
+			Integer bite =Integer.valueOf(line);
+			byte cos=bite.byteValue();
+			cosa=(char)cos;
+		} catch (Exception e) {
+			cosa=line.charAt(0);
+		}
 	System.out.println(cosa);
 }
 }
