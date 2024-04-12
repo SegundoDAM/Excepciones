4e0ef36a17868400682a510be13e084a37622fb0
pabloieshna <joesingleton417@gmail.com>
2024-04-12 09:00:54 +0200

    solucion final al conversion numero-letra

--- src/ejercicios/Ejercicio03.java
+++ src/ejercicios/Ejercicio03.java
@@ -3,19 +3,24 @@ package ejercicios;
 import java.util.Scanner;
 
 public class Ejercicio03 {
-public static void main(String[] args) {
-	Scanner leer=new Scanner(System.in);
-	char cosa;
-	String line;
-	byte nextInt = 0;
-		line= leer.nextLine();
+	public static void main(String[] args) {
+		Scanner leer = new Scanner(System.in);
+		char cosa;
+		String line;
+		char letra='n';
+		byte correspondencia=(byte) letra;
+		char otra=(char) correspondencia;
+		System.out.println("la letra "+letra+" es igual a "+otra);
+		line = leer.nextLine();
 		try {
-			Integer bite =Integer.valueOf(line);
-			byte cos=bite.byteValue();
-			cosa=(char)cos;
+			//intento hacer la conversion
+			Byte number = Byte.valueOf(line);
+			System.out.println("no hay fallo, luego es una letra");
+			cosa = (char) number.byteValue();
 		} catch (Exception e) {
-			cosa=line.charAt(0);
+			System.out.println("si hay fallo, luego es un numero");
+			cosa = line.charAt(0);
 		}
-	System.out.println(cosa);
-}
+		System.out.println(cosa);
+	}
 }
